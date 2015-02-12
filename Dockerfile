577d70597e6480251bd9bf154406b6853f5395e4
tifayuki <tifayuki@gmail.com>
2015-02-12 20:25:13 +0800

    inital commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM ubuntu:trusty
+MAINTAINER Feng Honglin <tifayuki@gmail.com>
+
+RUN apt-get update && \
+    apt-get install -y build-essential golang git mercurial && \
+    mkdir -p /release
+
+RUN git clone https://github.com/inconshreveable/ngrok.git ngrok
+
+ADD run.sh /run.sh
+ADD deploy_binary.sh /deploy_binary.sh
+RUN chmod +x /*.sh
+
+ENV TLS_KEY **None**
+ENV TLS_CERT **None**
+ENV CA_CERT **None**
+ENV DOMAIN **None**
+ENV TUNNEL_ADDR 4443
+ENV HTTP_ADDR 80
+ENV HTTPS_ADDR 443
+
+
+CMD ["/run.sh"]
---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+if [ "${TLS_KEY}" == "**None**" ]; then
+    echo "Please specify TLS_KEY"
+    exit 1
+fi
+
+if [ "${TLS_CERT}" == "**None**" ]; then
+    echo "Please specify TLS_CERT"
+    exit 1
+fi
+
+if [ "${CA_CERT}" == "**None**" ]; then
+    echo "Please specify CA_CERT"
+    exit 1
+fi
+
+if [ "${DOMAIN}" == "**None**" ]; then
+    echo "Please specify DOMAIN"
+    exit 1
+fi
+
+echo -e "${TLS_KEY}" > /server.key
+echo -e "${TLS_CERT}" > /server.crt
+echo -e "${CA_CERT}" > /ngrok/assets/client/tls/ngrokroot.crt
+
+
+echo "=> Compiling ngrok binary files"
+cd ngrok
+make release-server release-client
+cd /
+echo "=> Successfully build the binaries"
+
+/deploy_binary.sh
+
+echo "=> Running ngrok server"
+/ngrok/bin/ngrokd -tlsKey=/server.key -tlsCrt=/server.crt -domain="${DOMAIN}" -httpAddr=${HTTP_ADDR} -httpsAddr=${HTTPS_ADDR} -tunnelAddr=${TUNNEL_ADDR}
